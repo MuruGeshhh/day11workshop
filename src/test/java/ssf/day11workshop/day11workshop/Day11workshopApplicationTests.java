da0c31df8215219a4177fc5e2876c13f747c8824
MuruGeshhh <108484042+MuruGeshhh@users.noreply.github.com>
2022-07-18 13:50:54 +0800

    'master'

---
+++ src/test/java/ssf/day11workshop/day11workshop/Day11workshopApplicationTests.java
@@ -0,0 +1,13 @@
+package ssf.day11workshop.day11workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day11workshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
